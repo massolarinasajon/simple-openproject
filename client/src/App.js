713e6ef5d6d647c88bbf47c71e028b4fc6494b17
Douglas M <douglasmassolari@nasajon.com.br>
2019-05-14 16:53:28 -0300

    App com arrow function

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,13 @@ import 'bulma';
 import Table from './components/Table';
 import Filtro from './components/Filtro';
 
-function App() {
-    return (
-        <section className="section">
-            <div className="container">
-                <Filtro />
-                <Table />
-            </div>
-        </section>
-    );
-}
+const App = () => (
+    <section className="section">
+        <div className="container">
+            <Filtro />
+            <Table />
+        </div>
+    </section>
+);
 
 export default App;
